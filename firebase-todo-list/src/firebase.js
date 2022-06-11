d6342bca154fc492f89212c7a2e8b39b0e710096
Mohammad Faraz <farazmohammad190@gmail.com>
2022-06-11 17:04:01 +0500

    Practice Work

---
+++ firebase-todo-list/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "*******************************************",
+  authDomain: "******************************",
+  databaseURL: "************************************************",
+  projectId: "***************",
+  storageBucket: "************************",
+  messagingSenderId: "************",
+  appId: "************************************************"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const auth = getAuth();
